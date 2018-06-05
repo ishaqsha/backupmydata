b2915aee21e4cb6d36c50077126b228c1eb9655a
shaikh ishaque <shaikhishaque386@gmail.com>
2018-06-05 10:44:04 +0530

    addeda litttle endian to big endians

--- ff/littobig.c
+++ ff/littobig.c
@@ -1,29 +1,39 @@
- #include<stdio.h>
-main()
+#include<stdio.h>
+union A
 {
-int a=1,i,j,temp,b;
-b=a;
-char *p;
-p=(char *)&a;
-/*for(i=0;i<4;i++)
+
+	int a;
+
+
+}v;
+int main()
 {
-printf("%d   %p ",*p,p);
-p++;
-}*/
-if(*p==1)
-{
-printf("little endian it follow");
-printf("\n");
-for(i=0,j=3;i<j;i++,j--)
-{
-  temp=p[i];
-  p[i]=p[j];
-   p[j]=temp;
-}
-}
-printf("conerting little to big endian\n");
+	v.a=1;
+   // int	a=1;
+	int i,j,temp;
+	char *p;
+	p=(char *)&v.a;
+	//p=(char*)&a;
+printf("before converting\n");
 for(i=0;i<4;i++)
+{
+
 printf("%d",p[i]);
-printf("\n");
-printf("%d",b);
+
+}
+	printf("\n");
+	if(*p==0)
+	{
+		printf("big  endian it follow");
+		printf("\n");
+		for(i=0,j=3;i<j;i++,j--)
+		{
+			temp=p[i];
+			p[i]=p[j];
+			p[j]=temp;
+		}
+	}
+	printf("conerting little to big endian\n");
+	for(i=0;i<4;i++)
+		printf("%d",p[i]);
 }
